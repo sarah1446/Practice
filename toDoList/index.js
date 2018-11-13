1ea155b7f183fc4fe2b7c747f73466d131eabfc8
Sarah Yoon <lemonview2@gmail.com>
2018-11-13 22:37:12 +0900

    MOD - toDoList updated

--- toDoList/index.js
+++ toDoList/index.js
@@ -1,20 +1,26 @@
 //input내용입력  엔터  li생성 후 ul안에 넣음  input내용 삭제
-const textInput =  document.querySelector('.new-diary');
+const textInput = document.querySelector('.new-diary');
 const listBox = document.querySelector('.diary-list');
+//const delete ;
 
+const addList = function(e) {
+  if (e.keyCode !== 13) return;
+  const list = document.createElement('li');
+  const text = e.target.value; // textInput.value 같으니까..?
+  if (text.length == 0) {
+    alert('일정을 적어주세요!');
+    return;
+  }
+  list.innerHTML = `<span>${text}</span><span class='delete'>x</span>`;
+  console.log(list);
+  listBox.appendChild(list);
+  textInput.value = '';
 
-const addList = function(e){
-        if (e.keyCode !== 13) return;
-        //console.log(e.keyCode);
-        // console.log(e.target.value);
-        const list = document.createElement('li');
-        const text = e.target.value; // textInput.value 같으니까..?
-        //console.log(text);
-        //console.dir(list);
-        list.textContent = text;
-        console.log(list);
-        listBox.appendChild(list);
-        textInput.value = ''; //왜 innerText로 하면 안되지?
-    }
+  const dele = list.querySelector('.delete');
+  dele.addEventListener('click', function() {
+    alert('삭제하시겠습니까?');
+    this.closest('li').outerHTML = '';
+  });
+};
 
-textInput.addEventListener('keypress',addList);
+textInput.addEventListener('keypress', addList);
